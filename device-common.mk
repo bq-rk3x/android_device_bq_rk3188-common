10c8dfb216abaf241e3f92ffe68a01e6a4030400
fefifofum <fefifofum@email.com>
2014-02-15 18:02:06 +0100

    Wifi changes

--- device-common.mk
+++ device-common.mk
@@ -43,9 +43,11 @@ PRODUCT_COPY_FILES += \
     device/bq/rockchip-common/prebuilt/egl.cfg:system/lib/egl/egl.cfg \
     device/bq/rockchip-common/prebuilt/media_codecs.xml:system/etc/media_codecs.xml \
     device/bq/rockchip-common/prebuilt/nvram_RK903_26M.cal:system/etc/firmware/nvram_RK903_26M.cal \
+    device/bq/rockchip-common/prebuilt/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     device/bq/rockchip-common/prebuilt/rk29-keypad.kl:/system/usr/keylayout/rk29-keypad.kl \
     device/bq/rockchip-common/prebuilt/rk3xxx:system/bin/rk3xxx \
-    device/bq/rockchip-common/prebuilt/updater:obj/EXECUTABLES/updater_intermediates/updater
+    device/bq/rockchip-common/prebuilt/updater:obj/EXECUTABLES/updater_intermediates/updater \
+    device/bq/rockchip-common/prebuilt/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # Rootdir
 PRODUCT_COPY_FILES += \
