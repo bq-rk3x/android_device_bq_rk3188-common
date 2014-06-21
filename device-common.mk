6ed44a44dd9e9c2032e524e007b1c10bf0ff6817
fefifofum <jorsellis@gmail.com>
2014-06-21 04:58:23 +0200

    Remove deprecated mali files and options

--- device-common.mk
+++ device-common.mk
@@ -43,9 +43,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/prebuilt/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
     $(COMMON_PATH)/prebuilt/nvram_RK903_26M.cal:system/etc/firmware/nvram_RK903_26M.cal \
-    $(COMMON_PATH)/prebuilt/packages-compat.xml:system/etc/packages-compat.xml \
-    $(COMMON_PATH)/prebuilt/packages-composer.xml:system/etc/packages-composer.xml \
-    $(COMMON_PATH)/prebuilt/performance_info.xml:system/etc/performance_info.xml \
     $(COMMON_PATH)/prebuilt/rk29-keypad.kl:/system/usr/keylayout/rk29-keypad.kl \
     $(COMMON_PATH)/prebuilt/rk3x:system/bin/rk3x
 
@@ -97,7 +94,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     cm.updater.uri=http://bokerones.noip.me/CyanogenModOTA/api \
     ro.opengles.version=131072 \
-    ro.sf.lcdc_composer=0 \
     debug.hwui.render_dirty_regions=false \
     ro.config.facelock=enable_facelock \
     persist.facelock.detect_cutoff=5000 \
